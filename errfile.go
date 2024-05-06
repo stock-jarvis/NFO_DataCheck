9b9356bf29b870e2a61f893667b331a3762629bb
Ajab Raja <ajabraja9@gmail.com>
2024-05-06 19:53:35 +0530

    updated

--- errfile.go
+++ errfile.go
@@ -15,12 +15,14 @@ import (
 
 var (
 	threads = flag.Int("t", 350, "Number of routines to use. Do not exceede 400 on a 100 core system.")
-	source  = flag.String("s", "", "Path to source files.")
+	source1 = flag.String("s1", "", "Path to source files.")
+	source2 = flag.String("s2", "", "Path to errored original files.")
+	source3 = flag.String("s3", "", "Path to replaced files.")
 )
 
 func main() {
 	flag.Parse()
-	input_path := *source
+	input_path := *source1
 	if input_path == "" {
 		log.Fatal("No source path declared")
 	}
@@ -71,23 +73,58 @@ func CheckErr(inPath string) {
 			//log.Println("entered go func")
 			csvReader := csv.NewReader(file)
 			contents, _ := csvReader.ReadAll()
+			// file1, _ := os.Create("op.csv")
+			// csvWriter := csv.NewWriter(file1)
+			file1, _ := os.OpenFile(inPath, os.O_WRONLY, 1)
+			csvWriter1 := csv.NewWriter(file1)
+			name := strings.Split(inPath, "/")
+			outpath1 := fmt.Sprintf("/%v/%v", *source2, name[len(name)-1])
+			outpath2 := fmt.Sprintf("/%v/%v", *source3, name[len(name)-1])
+			file2, _ := os.Create(outpath1)
+			file3, _ := os.Create(outpath2)
+			defer file1.Close()
+			defer file2.Close()
+			defer file3.Close()
+			csvWriter2 := csv.NewWriter(file2) //copy of og files
+			csvWriter3 := csv.NewWriter(file3) //copy of replaced file
 			f := fmt.Sprint(filepath)
 			for j := 1; j < len(contents); j++ {
 				for i := 1; i < len(contents[j]); i++ {
+					original := []string{
+						fmt.Sprintf("%v", contents[j][0]),
+						fmt.Sprintf("%v", contents[j][1]),
+						fmt.Sprintf("%v", contents[j][2]),
+						fmt.Sprintf("%v", contents[j][3]),
+						fmt.Sprintf("%v", contents[j][4]),
+						fmt.Sprintf("%v", contents[j][5]),
+						fmt.Sprintf("%v", contents[j][6]),
+						fmt.Sprintf("%v", contents[j][7]),
+						fmt.Sprintf("%v", contents[j][8]),
+						fmt.Sprintf("%v", contents[j][9]),
+					}
+					csvWriter2.Write(original)
+					csvWriter2.Flush()
+
 					switch i {
 					case 1:
-						_, err := time.Parse("02/01/2006", strings.TrimSpace(contents[j][1]))
+						_, err := time.Parse("02/01/2006", contents[j][1])
 						if err != nil {
 							error_in_date = append(error_in_date, fmt.Sprintf("\nThread:%d , %v , Error in date %v, Row: %d  ", c, f, contents[j][1], j+1))
 						}
 
 					case 2:
-						ts, err := time.Parse("15:04:05", strings.TrimSpace(contents[j][2]))
+						ts, err := time.Parse("15:04:05", contents[j][2])
 						if err != nil {
-							error_in_ts = append(error_in_ts, fmt.Sprintf("\nThread:%d ,%v , Error in timescmp %v, Row: %d  ", c, f, contents[j][2], j+1))
+							error_in_ts = append(error_in_ts, fmt.Sprintf("\nThread:%d ,%v , Error in timestamp %v, Row: %d  ", c, f, contents[j][2], j+1))
 
 						}
 						h, m, _ := ts.Clock()
+						if h < 9 {
+							if h == 1 || h == 2 || h == 3 {
+								h += 12
+								ts = ts.Add(12 * time.Hour)
+							}
+						}
 						if h < 9 || h > 15 {
 							error_in_hours = append(error_in_hours, fmt.Sprintf("\nThread:%d ,%v , Error in hours %v, Row: %d  ", c, f, contents[j][2], j+1))
 						}
@@ -98,10 +135,30 @@ func CheckErr(inPath string) {
 							error_in_mins = append(error_in_mins, fmt.Sprintf("\nThread:%d ,%v , Hour = 15, Minutes > 29 %v, Row: %d  ", c, f, contents[j][2], j+1))
 
 						}
+
+						tsx := fmt.Sprint(ts)
+						tsy := strings.Split(tsx, " ")
+						replaced := []string{
+							fmt.Sprintf("%v", contents[j][0]),
+							fmt.Sprintf("%v", contents[j][1]),
+							fmt.Sprintf("%v", tsy[1]),
+							fmt.Sprintf("%v", contents[j][3]),
+							fmt.Sprintf("%v", contents[j][4]),
+							fmt.Sprintf("%v", contents[j][5]),
+							fmt.Sprintf("%v", contents[j][6]),
+							fmt.Sprintf("%v", contents[j][7]),
+							fmt.Sprintf("%v", contents[j][8]),
+							fmt.Sprintf("%v", contents[j][9]),
+						}
+						csvWriter1.Write(replaced)
+						csvWriter1.Flush()
+						csvWriter3.Write(replaced)
+						csvWriter3.Flush()
+
 					case 3:
 						_, err := strconv.ParseFloat(contents[j][3], 64)
 						if err != nil {
-							error_in_LTQ = append(error_in_LTQ, fmt.Sprintf("\nThread:%d ,%v , Error in LTP  %v, Row: %d  ", c, f, contents[j][3], j+1))
+							error_in_LTP = append(error_in_LTQ, fmt.Sprintf("\nThread:%d ,%v , Error in LTP  %v, Row: %d  ", c, f, contents[j][3], j+1))
 						}
 
 					case 4:
@@ -141,9 +198,10 @@ func CheckErr(inPath string) {
 							error_in_OI = append(error_in_OI, fmt.Sprintf("\nThread:%d ,%v , Error in OpenInterest %v, Row: %d  ", c, f, contents[j][9], j+1))
 						}
 					}
-
 				}
 			}
+
+			//csvWriter.Write(row)
 			wg.Done()
 			//tc++
 			//log.Printf("Thread closed %d", tc)
@@ -154,16 +212,16 @@ func CheckErr(inPath string) {
 	wg.Wait()
 	//log.Println("waiting...")
 
-	fmt.Printf("Errors in Date:\n %v ", error_in_date)
-	fmt.Printf("Errors in Timestamp:\n %v ", error_in_ts)
-	fmt.Printf("Errors in Timestamp Hours:\n %v ", error_in_hours)
-	fmt.Printf("Errors in Timestamp Mins:\n %v", error_in_mins)
-	fmt.Printf("Errors in LTP:\n %v", error_in_LTP)
-	fmt.Printf("Errors in Buy Price:\n %v", error_in_BP)
-	fmt.Printf("Errors in Buy Quantity:\n %v", error_in_BQ)
-	fmt.Printf("Errors in Sell Price:\n %v", error_in_SP)
-	fmt.Printf("Errors in Sell Quantity:\n %v", error_in_SQ)
-	fmt.Printf("Errors in LTQ:\n %v", error_in_LTQ)
-	fmt.Printf("Errors in Open Interest\n %v", error_in_OI)
+	log.Printf("\nErrors in Date:\n %v ", error_in_date)
+	log.Printf("\nErrors in Timestamp:\n %v ", error_in_ts)
+	log.Printf("\nErrors in Timestamp Hours:\n %v ", error_in_hours)
+	log.Printf("\nErrors in Timestamp Mins:\n %v", error_in_mins)
+	log.Printf("\nErrors in LTP:\n %v", error_in_LTP)
+	log.Printf("\nErrors in Buy Price:\n %v", error_in_BP)
+	log.Printf("\nErrors in Buy Quantity:\n %v", error_in_BQ)
+	log.Printf("\nErrors in Sell Price:\n %v", error_in_SP)
+	log.Printf("\nErrors in Sell Quantity:\n %v", error_in_SQ)
+	log.Printf("\nErrors in LTQ:\n %v", error_in_LTQ)
+	log.Printf("\nErrors in Open Interest\n %v", error_in_OI)
 
 }
